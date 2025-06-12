698cd05c97e5a76483fd577788f14c42b29c795a
jinj00oo <icems0428@naver.com>
2025-06-13 01:15:55 +0900

    feat: 온보딩 스택 네비게이션 및 로그인 페이지 전환 구현

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 
 export default function Index() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkOnboarding = async () => {
+      try {
+        const done = await AsyncStorage.getItem('onboardingDone');
+        if (done) {
+          //router.replace('/');
+          //온보딩 테스트용 주석
+          router.replace('/onboarding');
+        } else {
+          console.log('온보딩 체크 실패');
+          router.replace('/onboarding');
+        }
+      } catch (e) {
+        console.error('온보딩 체크 실패:', e);
+        router.replace('/onboarding');
+      } finally {
+        setLoading(false);
+      }
+    };
+    checkOnboarding();
+  }, []);
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>메인화면</Text>
     </View>
   );
 }
---
+++ app/onboarding/step2.tsx
@@ -0,0 +1,29 @@
+import { router } from 'expo-router';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export default function OnboardingStep2() {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
+      <Text>웨더타고 날씨에 따른</Text>
+      <Text>지하철 혼잡도 쉽게 알아봐요</Text>
+      <Text>자주 가는 역의 혼잡도를 알림으로 받아보세요</Text>
+      <Button
+        title="다음"
+        onPress={() => {
+          router.replace('/onboarding/step3');
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+});
