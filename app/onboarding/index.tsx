440a15475c79208dfeae7c01bc02823e6a53b0d2
Minjoo Kim <minpretty100@naver.com>
2025-06-19 22:34:19 +0900

    fix: index.tsx수정

--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -1,20 +1,32 @@
 import { router } from 'expo-router';
+import { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Index() {
   const insets = useSafeAreaInsets();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsReady(true);
+    }, 100); // 약간의 시간차로 안정성 확보
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom }]}>
       <Text>웨더타고 날씨에 따른</Text>
       <Text>지하철 혼잡도 쉽게 알아봐요</Text>
       <Text>자주 가는 역의 혼잡도를 알림으로 받아보세요</Text>
-      <Button
-        title="다음"
-        onPress={() => {
-          router.replace('/onboarding/step2');
-        }}
-      />
+      {isReady && (
+        <Button
+          title="다음"
+          onPress={() => {
+            router.replace('/onboarding/step2');
+          }}
+        />
+      )}
     </View>
   );
 }
