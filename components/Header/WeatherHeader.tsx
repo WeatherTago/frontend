caf98b799abd254132efb99b15cee5353de7578f
jinj00oo <icems0428@naver.com>
2025-06-28 13:43:20 +0900

    style: WeatherHeader 헤더 패딩 조정 및 폰트 테마 적용

--- components/Header/WeatherHeader.tsx
+++ components/Header/WeatherHeader.tsx
@@ -8,30 +8,27 @@ import AlarmDot from '../icons/AlarmDot';
 import StarIcon from '../icons/starIcon';
 
 const OuterContainer = styled.View`
-  padding-bottom: 8px;
   background-color: ${({ theme }) => theme.colors.gray[50]};
 `;
 const InnerContainer = styled.View`
   height: 54px;
-  padding: 0 14px;
+  padding: 8px 14px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
 `;
 
 const WeatherText = styled.Text`
-  font-family: 'Pretendard-ExtraBold';
+  font-family: ${({ theme }) => theme.fonts.pretendard.extrabold};
   font-weight: 800;
   font-size: 30px;
   line-height: 44px;
   color: ${({ theme }) => theme.colors.gray[300]};
 `;
 
-
 const BellWrapper = styled.View`
   position: relative;
 `;
-
 
 const IconButton = styled(TouchableOpacity)`
   width: 46px;
@@ -43,14 +40,12 @@ const IconButton = styled(TouchableOpacity)`
 const IconRow = styled.View`
   flex-direction: row;
   align-items: center;
-  
 `;
 const AlarmDotWrapper = styled.View`
   position: absolute;
   top: 5px;
   right: 5px;
 `;
-
 
 export default function WeatherHeader() {
   const insets = useSafeAreaInsets();
@@ -62,15 +57,23 @@ export default function WeatherHeader() {
         <WeatherText>WEATHER</WeatherText>
 
         <IconRow>
-          <IconButton onPress={() => { /* 즐겨찾기 로직 */ }}>
+          <IconButton
+            onPress={() => {
+              /* 즐겨찾기 로직 */
+            }}
+          >
             <StarIcon size={54} />
           </IconButton>
 
           <BellWrapper>
-            <IconButton onPress={() => { /* 공지사항 로직 */ }}>
+            <IconButton
+              onPress={() => {
+                /* 공지사항 로직 */
+              }}
+            >
               <Ionicons
                 name="notifications"
-                size={35} // 
+                size={35} //
                 color={theme.colors.gray[300]}
               />
             </IconButton>
@@ -83,4 +86,3 @@ export default function WeatherHeader() {
     </OuterContainer>
   );
 }
-
