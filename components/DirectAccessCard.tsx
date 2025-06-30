df1a16ad91d077627201865f74e3617290a6a3d7
Minjoo Kim <minpretty100@naver.com>
2025-06-30 22:40:13 +0900

    feat: home화면 구현 중

---
+++ components/DirectAccessCard.tsx
@@ -0,0 +1,68 @@
+import MediumButton from '@/components/Button/MediumButton';
+import { hp, px, wp } from '@/utils/scale';
+import { useTheme } from '@emotion/react';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+interface DirectAccessCardProps {
+  title: string;
+  subText: string;
+  buttonText: string;
+  onPress: () => void;
+}
+
+export default function DirectAccessCard({
+  title,
+  subText,
+  buttonText,
+  onPress,
+}: DirectAccessCardProps) {
+  const theme = useTheme();
+
+  return (
+    <View style={styles.card}>
+      <Text
+        style={{
+          fontSize: theme.typography.subtitle1.fontSize,
+          lineHeight: theme.typography.subtitle1.lineHeight,
+          fontWeight: theme.typography.subtitle1.fontWeight as any,
+          fontFamily: theme.fonts.pretendard.regular,
+          marginBottom: hp(8),
+          color: theme.colors.gray[900],
+        }}
+      >
+        {title}
+      </Text>
+
+      <Text
+        style={{
+          fontSize: theme.typography.body1.fontSize,
+          lineHeight: theme.typography.body1.lineHeight,
+          fontWeight: theme.typography.body1.fontWeight as any,
+          fontFamily: theme.fonts.pretendard.regular,
+          color: theme.colors.gray[400],
+          marginBottom: hp(16),
+        }}
+      >
+        {subText}
+      </Text>
+
+      <MediumButton
+        label={buttonText}
+        onPress={onPress}
+        backgroundColor={theme.colors.primary[700]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    padding: wp(26),
+    backgroundColor: '#fff',
+    borderRadius: px(16),
+    marginHorizontal:px(24),
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+});
