add413d035335b71fd802747d60f805aab028270
jinj00oo <icems0428@naver.com>
2025-07-22 16:56:11 +0900

    add: dependencies

--- react-natvie.config.js
+++ react-natvie.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   assets: ['./assets/fonts'],
+  dependencies: {
+    '@react-native-kakao/user': {
+      root: './node_modules/@react-native-kakao/user',
+    },
+    '@react-native-kakao/core': {
+      root: './node_modules/@react-native-kakao/core',
+    },
+  },
 };
